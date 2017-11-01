a9ebc950efcc2c78af3be024f0175fc534c87d58
warrd18-meet <warrd18@meet.mit.edu>
2017-11-01 18:53:07 +0200

    i did this and that

---
+++ lab1/lab1.py
@@ -0,0 +1,15 @@
+print("shira "*100)
+number1=6
+print(number1)
+number2=3
+print(number2)
+list=[1, 2, 3]
+s=0
+for i in range(3):
+    print(list[s]*3)
+    s=s+1
+a=2
+b=124
+c=3
+a+b+c=z
+print(z)
---
+++ lab2/lab2.py
@@ -0,0 +1,22 @@
+# def list():
+#     x=[5,10,15,20,25]
+#     y=[x[0],x[-1]]
+#     return(y)
+
+# print(list())
+
+
+# a=[1,1,2,3,5,8,13,21,34,55,89]
+# b=[]
+# for number in a:
+#      if number<5:
+#         b.append(number)
+# print (b)
+
+import.random
+
+a=[1,1,2,5,6,9,23,99]
+b=[1,1,3,4,5,88,99,100]
+for value in a:
+    if value in b:
+        print(value)
