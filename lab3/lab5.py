86c0584c29b2764c8abf6ed7dacf31bf023e5267
warrd18-meet <warrd18@meet.mit.edu>
2017-11-29 19:22:27 +0200

    lab 5

--- lab3/lab5.py
+++ lab3/lab5.py
@@ -13,8 +13,11 @@ class Square(Turtle):
 		self.color((r,g,b))
 		
 
-square1 = Square(15)
-square1.random_color()
+# square1 = Square(15)
+# square1.random_color()
+
+
+
 
 class Hexagon(Turtle):
 	def __init__ (self,size):
@@ -26,7 +29,42 @@ class Hexagon(Turtle):
 
 		self.end_poly() 
 		y = self.get_poly()
-		self.register_shape("y",y) 
+		register_shape("y",y) 
 		self.shape("y")
-hexagone1 = Hexagon(20)
+	def random_color(self):
+		red = random.randint(0,256)
+		green = random.randint(0,256)
+		blue = random.randint(0,256)
+		self.color((red,green,blue))		
+# hexagone1 = Hexagon(20)
+# hexagone1.random_color()
+
+class Rectangle(Turtle):
+	def __init__ (self,width,hight):
+		Turtle.__init__(self)
+		register_shape("rectan",((width,0),(width,hight),(0,hight),(0,0)))
+		self.shape ("rectan")
+		self.shapesize()
+	def random_color(self):
+		re = random.randint(0,256)
+		gr = random.randint(0,256)
+		bl = random.randint(0,256)
+		self.color((re,gr,bl))
+
+#Rectangle1 = Rectangle(20,40)
+#Rectangle1.random_color()
+
+class Square1(Rectangle):
+	def __init__ (self,size):
+		Rectangle.__init__(self,size,size)
+	def random_color(self):
+		re1 = random.randint(0,256)
+		gr1= random.randint(0,256)
+		bl1 = random.randint(0,256)
+		self.color((re1,gr1,bl1))
+
+new_square =  Square1(20)
+new_square.random_color()
+
+
 mainloop()
